80309df6d77b71bf5917053347b4556855402aca
Lynn7730 <82749445+Lynn7730@users.noreply.github.com>
2023-10-16 17:40:00 +0800

    Working code

--- ArrayList-Lab-091023/Lab-Question-2/Main.java
+++ ArrayList-Lab-091023/Lab-Question-2/Main.java
@@ -5,35 +5,34 @@ class Main {
 
         Payroll pr = new Payroll();
 
-        // Test
-        String hours = JOptionPane.showInputDialog("Enter employee " + pr.getEmployeeId(0) + " hours worked.");
-        String payRate = JOptionPane.showInputDialog("Enter employee " + pr.getEmployeeId(0) + " pay rate.");
-        int intHours = Integer.parseInt(hours);
-        double doublePayRate = Double.parseDouble(payRate);
-        pr.setHours(0, intHours);
-        pr.setPayRate(0, doublePayRate);
-
-        pr.calcWages(pr.getEmployeeId(0));
-        System.out.println("Employee " + pr.getEmployeeId(0) + " wages is " + pr.getWages(0));
-
         // Main code
-        /*
-         * for (int i = 0; i < pr.employeeId.size(); i++) {
-         * String hours = JOptionPane.showInputDialog("Enter employee " +
-         * pr.getEmployeeId(i) + " hours worked.");
-         * String payRate = JOptionPane.showInputDialog("Enter employee " +
-         * pr.getEmployeeId(i) + " pay rate.");
-         * int intHours = Integer.parseInt(hours);
-         * double doublePayRate = Double.parseDouble(payRate);
-         * pr.setHours(i, intHours);
-         * pr.setPayRate(i, doublePayRate);
-         * }
-         * 
-         * for (int x = 0; x < pr.employeeId.size(); x++) {
-         * pr.calcWages(pr.getEmployeeId(x));
-         * System.out.println("Employee " + pr.getEmployeeId(x) + " wages is " +
-         * pr.getWages(x));
-         * }
-         */
+        for (int i = 0; i < pr.employeeId.size(); i++) {
+            String hours = JOptionPane.showInputDialog("Enter employee " + pr.getEmployeeId(i) + " hours worked.");
+            int intHours = Integer.parseInt(hours);
+            while (intHours <= 0) {
+                JOptionPane.showMessageDialog(null, "Employee working hours must not be zero or a negative number",
+                        "Error", JOptionPane.ERROR_MESSAGE);
+                hours = JOptionPane.showInputDialog("Enter employee " + pr.getEmployeeId(i) + " hours worked.");
+                intHours = Integer.parseInt(hours);
+            }
+
+            String payRate = JOptionPane.showInputDialog("Enter employee " + pr.getEmployeeId(i) + " pay rate.");
+            double doublePayRate = Double.parseDouble(payRate);
+            while (doublePayRate < 6.0) {
+                JOptionPane.showMessageDialog(null, "Employee pay rate must not be less than 6.0",
+                        "Error", JOptionPane.ERROR_MESSAGE);
+                payRate = JOptionPane.showInputDialog("Enter employee " + pr.getEmployeeId(i) + " pay rate.");
+                doublePayRate = Double.parseDouble(payRate);
+            }
+
+            pr.setHours(i, intHours);
+            pr.setPayRate(i, doublePayRate);
+        }
+
+        for (int x = 0; x < pr.employeeId.size(); x++) {
+            pr.calcWages(pr.getEmployeeId(x));
+            JOptionPane.showMessageDialog(null, "Employee " + pr.getEmployeeId(x) + " wages is " +
+                    pr.getWages(pr.getEmployeeId(x)), "Wages", JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 }
